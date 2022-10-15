a1a3ef3729781026f62a3adb51965dd3265dab51
nicolas <nicolas.sabas@epitech.eu>
2022-10-15 11:56:09 -0400

    - test

---
+++ tests/RequestManagerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Tests;
+
+use App\API\RequestManager;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class RequestManagerTest extends TestCase
+{
+    public function testPathResovler()
+    {
+        $httpClient = $this->createMock(HttpClientInterface::class);
+        $serializer = $this->createMock(SerializerInterface::class);
+        $cache = $this->createMock(CacheInterface::class);
+
+        $requestManager = new RequestManager($httpClient, $serializer, $cache);
+
+        $path = $requestManager->resolvePath(
+            [
+                RequestManager::METHOD => Request::METHOD_GET,
+                RequestManager::PATH => '/api/search/{uid}'
+            ],
+            [
+                '{uid}' => 'je-suis-un-uid'
+            ]
+        );
+
+        $this->assertStringNotContainsString('{uid}', $path);
+
+    }
+}
