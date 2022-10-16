6d34937ddd8eccfd90de92088306b8343ab7425e
nicolas <nicolas.sabas@epitech.eu>
2022-10-16 17:31:56 -0400

    - add doc annotation

--- src/API/Action/Image/ImageAction.php
+++ src/API/Action/Image/ImageAction.php
@@ -4,15 +4,31 @@ namespace App\API\Action\Image;
 
 use App\API\RequestManager;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
+/**
+ * Class ImageAction
+ * @package App\API\Action\Image
+ * @author Nicolas SABAS <nicolassabas.freelance@gmail.com>
+ */
 class ImageAction
 {
 
+    /**
+     * @var RequestManager
+     */
     private RequestManager $requestManager;
 
+    /**
+     * @param RequestManager $requestManager
+     */
     public function __construct(
         RequestManager $requestManager
     )
@@ -22,7 +38,14 @@ class ImageAction
 
 
     /**
+     * @param string $name
+     * @param string $size
      * @return Response
+     *
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
      *
      * @Route("/api/image/{name}/{size}", name="image_render", methods={"GET"})
      *
